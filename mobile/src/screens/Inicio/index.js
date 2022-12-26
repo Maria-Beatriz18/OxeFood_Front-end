bf51f2f6e096d7ff2281642338760e963baf2720
lsguilherme <lacerdagui42@gmail.com>
2022-12-26 16:02:58 -0300

    Criação do projeto base

---
+++ mobile/src/screens/Inicio/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { styles } from './styles';
+
+export function Inicio() {
+    return (
+        <View style={styles.container}>
+          <Text>TESTE - TELA INICIAL</Text>
+        </View>
+      );
+}
---
+++ mobile/src/screens/Inicio/styles.js
@@ -0,0 +1,7 @@
+import { StatusBar, StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    paddingTop: StatusBar.currentHeight,
+  }
+});
