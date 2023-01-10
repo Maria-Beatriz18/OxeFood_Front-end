d8e5bf50acb9458bb2e2ca8cfa73152e7f87b22b
lsguilherme <lacerdagui42@gmail.com>
2023-01-10 11:27:42 -0300

    Autenticação de login e Cadastro de Usuario

--- mobile/src/screens/CadastroUsuario/index.js
+++ mobile/src/screens/CadastroUsuario/index.js
@@ -1,5 +1,6 @@
+import axios from "axios";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -13,6 +14,33 @@ import { Button, Header, Icon } from "react-native-elements";
 import { styles } from "./styles";
 
 export function CadastroUsuario({ navigation }) {
+  const API_URL = `https://oxefood-backend-production.up.railway.app/api/cliente`;
+
+  const [email, setEmail] = useState();
+  const [numero, setNumero] = useState();
+  const [nome, setNome] = useState();
+  const [sobrenome, setSobrenome] = useState();
+  const [senha, setSenha] = useState();
+
+  async function salvar() {
+    if (email && numero && nome && sobrenome && senha) {
+      await axios
+        .post(API_URL, {
+          perfilUrl: "",
+          email: email,
+          fone: numero,
+          nome: nome,
+          password: senha,
+          sobrenome: sobrenome,
+        })
+        .then(function (response) {
+          alert("Cadastrado");
+        })
+        .catch((err) => alert(err));
+    } else {
+      alert("vazio");
+    }
+  }
   return (
     <KeyboardAvoidingView style={styles.container}>
       <Header
@@ -61,30 +89,50 @@ export function CadastroUsuario({ navigation }) {
           style={styles.input}
           placeholder="Email"
           autoCorrect={false}
+          value={email}
+          onChangeText={(value) => setEmail(value)}
+          keyboardType={"email-address"}
         />
 
         <TextInput
           style={styles.input}
           placeholder="Número"
           autoCorrect={false}
+          value={numero}
+          onChangeText={(value) => setNumero(value)}
+          keyboardType={"numeric"}
+          maxLength={20}
         />
 
         <TextInput
           style={styles.input}
-          placeholder="Nome completo"
+          placeholder="Nome"
           autoCorrect={false}
+          value={nome}
+          onChangeText={(value) => setNome(value)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Sobrenome"
+          autoCorrect={false}
+          value={sobrenome}
+          onChangeText={(value) => setSobrenome(value)}
         />
 
         <TextInput
           style={styles.input}
           placeholder="Senha"
           autoCorrect={false}
+          value={senha}
+          onChangeText={(value) => setSenha(value)}
+          secureTextEntry={true}
         />
+
         <Button
           title="Criar Conta"
           containerStyle={{ marginTop: 20 }}
           buttonStyle={styles.button}
-          onPress={() => navigation.navigate("Login")}
+          onPress={() => salvar()}
         />
       </View>
     </KeyboardAvoidingView>
--- mobile/src/screens/Login/index.js
+++ mobile/src/screens/Login/index.js
@@ -1,50 +1,87 @@
-import React from 'react';
-import { TouchableHighlight, View, Text, TouchableOpacity } from 'react-native';
-import { Button, Header, Image, Input } from 'react-native-elements';
+import axios from "axios";
+import React, { useState } from "react";
+import {
+  TouchableHighlight,
+  View,
+  Text,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import { Button, Header, Image, Input } from "react-native-elements";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
 export function Login({ navigation }) {
+  const API_URL = `https://oxefood-backend-production.up.railway.app/api/login/signin`;
+
+  const [email, setEmail] = useState();
+  const [senha, setSenha] = useState();
+
+  async function login() {
+    if (email && senha) {
+      await axios
+        .post(API_URL, {
+          username: email,
+          password: senha,
+        })
+        .then(function (response) {
+          setEmail(""), setSenha("");
+          if (response.data.token) {
+            navigation.navigate("HomeScreen", {
+              token: response.data.token,
+              id: response.data.userid,
+            });
+          }
+        })
+        .catch((err) => alert("Usuário incorreto"));
+    } else {
+      Alert.alert("Erro", "Email ou senha inválidos.")
+    }
+  }
   return (
     <View style={styles.container}>
-        <Header
-            centerComponent={
-            <Image
-                source={require('../../assets/header-login.png')}
-                style={{width:220, height:100}}
-            />}
+      <Header
+        centerComponent={
+          <Image
+            source={require("../../assets/header-login.png")}
+            style={{ width: 220, height: 100 }}
+          />
+        }
+        backgroundColor="#C60303"
+      />
 
-            backgroundColor='#C60303'
-        />
+      <Input
+        label="E-mail"
+        placeholder="E-mail"
+        inputContainerStyle={styles.input}
+        containerStyle={[styles.inputContainer, { marginTop: 64 }]}
+        value={email}
+        onChangeText={(value) => setEmail(value)}
+      />
 
-        <Input
-            label='E-mail'
-            placeholder='E-mail'
-            inputContainerStyle={styles.input}
-            containerStyle={[styles.inputContainer, {marginTop:64}]}
-        />
+      <Input
+        label="Senha"
+        placeholder="Senha"
+        inputContainerStyle={styles.input}
+        containerStyle={styles.inputContainer}
+        secureTextEntry={true}
+        value={senha}
+        onChangeText={(value) => setSenha(value)}
+      />
 
-        <Input
-            label='Senha'
-            placeholder='Senha'
-            inputContainerStyle={styles.input}
-            containerStyle={styles.inputContainer}
-            secureTextEntry={true}
-        />
+      <TouchableOpacity
+        style={{ marginRight: 180, marginTop: -8 }}
+        onPress={() => navigation.navigate("EsqueceuSenha")}
+      >
+        <Text>Esqueceu sua senha?</Text>
+      </TouchableOpacity>
 
-        <TouchableOpacity 
-            style={{marginRight:180, marginTop:-8}}
-            onPress={()=>navigation.navigate('EsqueceuSenha')}
-        >
-            <Text>Esqueceu sua senha?</Text>
-        </TouchableOpacity>
-
-        <Button 
-            title='Entrar'
-            containerStyle={{marginTop: 80}}
-            buttonStyle={styles.button}
-            onPress={()=>navigation.navigate('HomeScreen')}
-        />
+      <Button
+        title="Entrar"
+        containerStyle={{ marginTop: 80 }}
+        buttonStyle={styles.button}
+        onPress={() => login()}
+      />
     </View>
   );
 }
